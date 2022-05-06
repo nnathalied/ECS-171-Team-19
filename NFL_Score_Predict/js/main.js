a27216135d6b94dfca5db033729702037cef99dc
rookieCoderLin <71912011+rookieCoderLin@users.noreply.github.com>
2022-05-06 16:30:37 -0700

    Front-End stage one
    This is stage one of the front end, including the home page, predict page, and about page. The programming languages/tools used in stage one are HTML, CSS, Javascript, and Jquery. Stage one does not have a user interaction with the data and models as they will be implemented in front-end stage two.

---
+++ NFL_Score_Predict/js/main.js
@@ -0,0 +1,8 @@
+
+	$(document).ready(function() {
+	});
+
+
+	String.prototype.replaceAll = function(str1, str2, ignore) {
+    	return decodeURIComponent( this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2) );
+	} 
